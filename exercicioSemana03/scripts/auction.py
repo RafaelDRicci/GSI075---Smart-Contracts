6c733b420339d31fb93f1069d22aa0bf5a354e0c
RafaelDRicci <rafael_d_ricci@hotmail.com>
2021-12-19 17:37:20 -0300

    Commit do código da aula da terceira semana e a primeira parte do exercício

---
+++ exercicioSemana03/scripts/auction.py
@@ -0,0 +1,77 @@
+from brownie import *
+import brownie
+
+def main():
+
+    contractOwner = accounts[0]
+    tokenOwner = accounts[1]
+
+    bidder1 = accounts[2]
+    bidder2 = accounts[3]
+    bidder3 = accounts[4]
+    bidderFake = accounts[5]
+
+    token = VerySimpleToken.deploy("Token1",{'from': tokenOwner})
+    auction = TokenAuction.deploy(100,50,{'from': contractOwner})
+
+    #A taxa é alterada para 80 pelo dono do contrato
+    auction.changeFee(80, {'from':contractOwner})
+    try:
+    #O dono do token tenta altera a taxa para 50, mas não consegue e é lançada uma exception
+        auction.changeFee(50, {'from':tokenOwner})
+    except:
+        print("Dont!!")
+
+    auction.createAuction("Leilao1", 12, token,{'from':tokenOwner})
+
+    token.transfer(auction,{'from':tokenOwner})
+
+    auction.initAuction("Leilao1")
+
+    auction.sendCollateral("Leilao1",{'from':bidder1,'value':100})
+    auction.sendCollateral("Leilao1",{'from':bidder2,'value':100})
+    auction.sendCollateral("Leilao1",{'from':bidder3,'value':100})
+
+
+    auction.bid("Leilao1",1000,{'from':bidder1})
+    auction.bid("Leilao1",1010,{'from':bidder2})
+    auction.bid("Leilao1",1020,{'from':bidder3})
+    auction.bid("Leilao1",1010,{'from':bidder1})
+    auction.bid("Leilao1",1050,{'from':bidder2})
+
+    try:
+        auction.bid("Leilao1",1099,{'from':bidderFake})
+    except:
+        print("Dont!!")
+
+
+    try:
+        auction.bid("Leilao1",1000,{'from':bidder1})
+        auction.bid("Leilao1",1000,{'from':bidder1})
+        auction.bid("Leilao1",1000,{'from':bidder1})
+        auction.bid("Leilao1",1000,{'from':bidder1})
+        auction.bid("Leilao1",1000,{'from':bidder1})
+    except:
+        print("Dont!!")
+
+    auction.claimToken("Leilao1",{'from':bidder2,'value':1050-100})
+
+
+    auction.claimCollateral("Leilao1",{'from':bidder1})
+    try:
+        auction.claimCollateral("Leilao1",{'from':bidder2})
+    except:
+        print('Tentei roubar...mas nao consegui...')
+
+    auction.claimCollateral("Leilao1",{'from':bidder3})
+    auction.getProfit("Leilao1",{'from':contractOwner})
+    auction.getFee({'from':tokenOwner})
+
+    #Utilizando a função auxiliar para demonstrar o valor da taxa ao final com os demais resultados
+    print(auction.verifyFee())
+    print(bidder1.balance())
+    print(bidder2.balance())
+    print(token.isOwner.call(bidder2))
+    print(bidder3.balance())
+    print(contractOwner.balance())
+    print(tokenOwner.balance())
